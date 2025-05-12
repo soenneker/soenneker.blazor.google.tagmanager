b1dfa58c38d5c3457991fc44b7ad46fdde329226
Jake Soenneker <jake@soenneker.com>
2025-05-12 14:35:15 -0500

    script loading changes

--- src/wwwroot/js/googletagmanagerinterop.js
+++ src/wwwroot/js/googletagmanagerinterop.js
@@ -1,28 +1,26 @@
 ﻿export class GoogleTagManagerInterop {
-        init(gtmId) {
+    init(gtmId) {
         if (window.dataLayer && window.dataLayer.find(e => e.event === "gtm.js")) {
             return;
         }
 
-        const scriptTag = document.createElement('script');
-        scriptTag.textContent = `
+        const inlineScript = document.createElement('script');
+        inlineScript.innerHTML = `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
             j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
             f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','${gtmId}');
-            `;
-        document.head.appendChild(scriptTag);
+        `;
+        document.head.appendChild(inlineScript);
 
-        const iframe = document.createElement('iframe');
-        iframe.src = `https://www.googletagmanager.com/ns.html?id=${gtmId}`;
-        iframe.style.display = "none";
-        iframe.style.visibility = "hidden";
-        iframe.width = "0";
-        iframe.height = "0";
-
-        document.body.prepend(iframe);
+        const noscript = document.createElement('noscript');
+        noscript.innerHTML = `
+            <iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        `;
+        document.body.prepend(noscript);
     }
 
     pushEvent(eventData) {
